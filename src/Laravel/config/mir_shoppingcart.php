0cf6474448b149c316eb100c14dc6344ceb9b6eb
mojtaba shafizadeh <mojtaba031@yahoo.com>
2023-04-23 11:23:12 +0330

    Add files via upload

---
+++ src/Laravel/config/mir_shoppingcart.php
@@ -0,0 +1,11 @@
+<?php
+    return [
+		'sms' => [
+			'website'    => 'https://www.niazpardaz-sms.com',
+			'webservice' => 'http://payamak-service.ir/SendService.svc?wsdl',
+			'username'   => '',
+			'password'   => '',
+		],
+
+    ];
+?>
