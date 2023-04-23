f3fec948c55e3dcc113e3bf513319c83d791f283
mojtaba shafizadeh <mojtaba031@yahoo.com>
2023-04-23 11:43:48 +0330

    Add files via upload

---
+++ src/Laravel/ShoppingCartServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace NiazPardaz\Sms\Laravel;
+
+use Illuminate\Support\ServiceProvider;
+use NiazPardaz\Sms\NiazPardaz;
+
+class ShoppingCartServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        // $this->mergeConfigFrom(__DIR__.'/config/niazpardaz.php', 'niazpardaz' );
+    }
+
+    public function boot()
+    {
+        // $this->publishes([ __DIR__.'/config/niazpardaz.php' => config_path('niazpardaz.php') ]);
+        // $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+        $this->app->singleton('ShoppingCart', function(){
+            return new ShoppingCart();
+        });
+    }
+}
---
+++ src/ShoppingCart.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Mir\ShoppingCart;
+
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Cookie;
+
+class Cart
+{
+    private $cookie_time;
+    private $cookie_name;
+    private $cookie_id;
+
+    public function __construct() {
+        $this->cookie_name = "cart";
+        $this->cookie_time = 86400;
+    }
+
+    public function hasCookie( $text  ){
+        if( Cookie::get( $this->cookie_name ) !== null ){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function getCookie(){
+        return Cookie::get( $this->cookie_name );
+    }
+
+    public function addCookie( $time = $this->cookie_time ){
+        $CookieID = Str::random(10);
+        Cookie::queue( $this->cookie_name, $CookieID, $time );
+    }
+}
